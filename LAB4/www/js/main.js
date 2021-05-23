1a0c9cfef623cfb64a4b1811720b372fb06759bb
niawgai <ror3215@gmail.com>
2021-05-23 15:48:33 +0700

    LAB4

---
+++ LAB4/www/js/main.js
@@ -0,0 +1,39 @@
+console.log("Hello World")
+// alert("Niaw");
+
+var name = "Niaw"
+let Age = 21;
+
+console.log("My name is " + name)
+
+name = [5,6,7];
+name[0] = 10;
+name[2] = "Win";
+
+document.getElementById("content").innerHTML = name;
+
+// {"Key":"value"}
+// users{"name":"Win"},{"name":"Boy"}
+var users= '{"name":"Niaw","name":"Boy"}';
+var json = JSON.parse(users);
+console.log(json);
+
+emp = { name: "AA" , name: "BB" , name : "CC" };
+console.log(emp);
+console.log(emp.name);
+
+if(name !=5){
+    console.log("True");
+}else{
+    console.log("False, name is 5");
+}
+
+for(i =1; i<5 ; i++){
+    console.log(i);
+}
+
+console.log(Add(5,3));
+function Add(A,B) {
+    c = A+B;
+    return c;
+}
